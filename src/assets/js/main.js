e292e109b82641cb0377c482fd260a0204eb42dc
AlexIstra1989 <alex888fcsm@gmail.com>
2021-11-16 23:56:39 +0300

    fix footer

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,22 @@
 
 $(document).ready(function(){
- $('.slick').slick({
+ $('.slick__box').slick({
    dots: true,
    slidesToShow: 1,
-   slidesToScroll: 1
+   slidesToScroll: 1,
+   mobileFirst: true,
+    // responsive: [
+    //  {
+    //  breakpoint: 1280, 
+    //  settings: "unslick"
+    //  },
+    //  // {
+    //  //  breakpoint: 1280, 
+    //  //  settings: {
+    //  //   slidesToShow: 1,
+    //  //   slidesToScroll: 1,
+    //  //  }
+    //  //  }
+    // ]
  });
-});
+}); 
